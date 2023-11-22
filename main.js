b30cc3b84039f4ca3bcea13cf42ec7d7ef7fbd35
Hashim Almuqbel <85612244+AMAZINGMAN2@users.noreply.github.com>
2023-11-22 18:50:02 +1030

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ const chars= "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdf
 
 var t = setInterval(function() { 
 let thestring = "";
-for(let i = 0 ; i <20000; i++)
+for(let i = 0 ; i <9964; i++)
 {
 var randomNumber = Math.round( Math.random() * (chars.length-1) );
 thestring += chars[randomNumber];
 }
 const frt = document.getElementById('charChanger');
 frt.innerHTML = thestring;
-}, 100); 
+}, 25); 
