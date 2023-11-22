b3f1eac73477edfc9e698daa638e3b0216a3839c
Hashim Almuqbel <85612244+AMAZINGMAN2@users.noreply.github.com>
2023-11-22 18:17:23 +1030

    Add files via upload
    main commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const chars= "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM/;[]\\{}|1234567890!@#$%^&*()-=+:";
+
+
+var t = setInterval(function() { 
+let thestring = "";
+for(let i = 0 ; i <20000; i++)
+{
+var randomNumber = Math.round( Math.random() * (chars.length-1) );
+thestring += chars[randomNumber];
+}
+const frt = document.getElementById('charChanger');
+frt.innerHTML = thestring;
+}, 100); 
